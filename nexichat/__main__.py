788605b976d18a1d6562b9d57b6cb6b6d1f7ed1c
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-10-13 21:08:23 +0530

    Update __main__.py

--- nexichat/__main__.py
+++ nexichat/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 import importlib
 
 from pyrogram import idle
+from pyrogram.types import BotCommand
 from config import OWNER_ID
 from nexichat import LOGGER, nexichat
 from nexichat.modules import ALL_MODULES
@@ -18,11 +19,34 @@ async def anony_boot():
         importlib.import_module("nexichat.modules." + all_module)
         LOGGER.info(f"Successfully imported : {all_module}")
 
+    # Set bot commands
+    try:
+        await nexichat.set_bot_commands(
+            commands=[
+                BotCommand("start", "Start the bot"),
+                BotCommand("help", "Get the help menu"),
+                BotCommand("ping", "Check if the bot is alive or dead"),
+                BotCommand("lang", "Select bot reply language"),
+                BotCommand("resetlang", "Reset to default bot reply lang"),
+                BotCommand("id", "Get users user_id"),
+                BotCommand("stats", "Check bot stats"),
+                BotCommand("gcast", "Broadcast any message to groups/users"),
+                BotCommand("chatbot", "Enable or disable chatbot"),
+                BotCommand("shayri", "Get random shayri for love"),
+                BotCommand("repo", "Get chatbot source code"),
+                BotCommand("ask", "Ask anything question by ai"),
+            ]
+        )
+        LOGGER.info("Bot commands set successfully.")
+    except Exception as ex:
+        LOGGER.error(f"Failed to set bot commands: {ex}")
+
     LOGGER.info(f"@{nexichat.username} Started.")
     try:
         await nexichat.send_message(int(OWNER_ID), f"{nexichat.mention} has started")
     except Exception as ex:
-        LOGGER.info(f"@{nexichat.first_name} Started, please start the bot from owner id.")
+        LOGGER.info(f"@{nexichat.username} Started, please start the bot from owner id.")
+    
     await idle()
 
 
